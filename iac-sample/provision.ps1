2686e333d9708c5be869c3ba5111b6e7408ec49b
Manoj Mishra <manoj.tec@gmail.com>
2026-02-05 15:28:06 +0530

    Update provision.ps1

--- iac-sample/provision.ps1
+++ iac-sample/provision.ps1
@@ -1,1 +1,15 @@
+Configuration SampleADUserProvisioning {
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+    Node "localhost" {
+        User DemoUser {
+            UserName = "demo.user"
+            Password = (ConvertTo-SecureString "P@ssw0rd!" -AsPlainText -Force)
+            Ensure = "Present"
+            FullName = "Demo User"
+            Description = "Sample IaC provisioned user"
+        }
+    }
+}
 
+# Generate MOF file
+SampleADUserProvisioning -OutputPath ./DSC
